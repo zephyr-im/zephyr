6b718b0452a74d1388e51ceb767a869a9059bc13
Kenneth G Raeburn <raeburn@mit.edu>
1991-02-28 18:31:41 +0000

    If MPROF is defined, use standard C library memory allocator.

--- server/zalloc.h
+++ server/zalloc.h
@@ -1,5 +1,14 @@
 /*
  */
 
+#ifdef MPROF
+inline void * zalloc (unsigned int sz) {
+    return malloc (sz);
+}
+inline void zfree (void *ptr, unsigned int sz) {
+    free (ptr);
+}
+#else
 extern void * zalloc (unsigned int);
 extern void zfree (void *, unsigned int);
+#endif
