43dfbf86e548feaa629ec1989d7b7c8ffe5dc41f
John F. Carr <jfc@mit.edu>
1991-03-08 08:49:51 +0000

    AIX 1.2 defines ulong in <sys/types.h>

--- clients/zwgc/ulong.h
+++ clients/zwgc/ulong.h
@@ -12,13 +12,14 @@
  *      For copying and distribution information, see the file
  *      "mit-copyright.h".
  */
-
-
-#include <zephyr/mit-copyright.h>
-
 #ifndef ulong_MODULE
 #define ulong_MODULE
 
+#if defined(_AIX) && defined(i386)
+/* AIX 1.2 defines ulong in <sys/types.h>.  */
+#include <sys/types.h>
+#else
 typedef unsigned long ulong;
+#endif
 
 #endif
