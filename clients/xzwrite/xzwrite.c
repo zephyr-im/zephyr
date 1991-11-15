a276f1acb8cd7e98409029348f67f9c150a6ceb5
Barry Jaspan <bjaspan@mit.edu>
1991-11-15 13:25:51 +0000

    use passwd information when no signature is defined

--- clients/xzwrite/xzwrite.c
+++ clients/xzwrite/xzwrite.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pwd.h>
 
 #include "xzwrite.h"
 
@@ -8,25 +9,15 @@ main(argc, argv)
    int	argc;
    char	**argv;
 {
+     char sigbfr[BUFSIZ];
+     
      zeph_init();
 
      build_interface(&argc, argv);
 
      if (argc > 1) usage();
 
-     /* Do magic with signature */
-     if (! *defs.signature) {
-	  char *sig;
-
-	  sig = (char *) zeph_get_signature();
-	  if (sig) {
-	       defs.signature = (char *) Malloc(strlen(sig) + 1,
-						"getting signature",
-						NULL);
-	       strcpy(defs.signature, sig);
-	  }
-     }
-	 
+     set_signature();
      dest_init();
      yank_init();
      edit_win_init();
@@ -41,6 +32,49 @@ main(argc, argv)
 
      go();
 }
+
+set_signature()
+{
+     char *sig, sigbfr[BUFSIZ];
+     
+     /* Do magic with signature */
+     if (*defs.signature)
+	  return;
+
+     sig = (char *) zeph_get_signature();
+     if (!sig) {
+	  /* try to find name in the password file */
+	  register struct passwd *pwd;
+	  register char *cp = sigbfr;
+	  register char *cp2, *pp;
+	  
+	  pwd = getpwuid(getuid());
+	  if (pwd) {
+	       cp2 = pwd->pw_gecos;
+	       for (; *cp2 && *cp2 != ',' ; cp2++) {
+		    if (*cp2 == '&') {
+			 pp = pwd->pw_name;
+			 *cp++ = islower(*pp) ? toupper(*pp) : *pp;
+			 pp++;
+			 while (*pp)
+			      *cp++ = *pp++;
+		    } else
+			 *cp++ = *cp2;
+	       }
+	       *cp = '\0';
+	       sig = sigbfr;
+	  }
+     }	
+     
+     if (sig) {
+	  defs.signature = (char *) Malloc(strlen(sig) + 1,
+					   "getting signature",
+					   NULL);
+	  strcpy(defs.signature, sig);
+     }
+}
+	 
+
 
 usage()
 {
