d063f72abdd328f73837612759f2429679a1297f
Greg Hudson <ghudson@mit.edu>
1995-06-30 18:32:22 +0000

    Oops, forgot this file (needed for autoheader).

---
+++ acconfig.h
@@ -0,0 +1,24 @@
+/* Define to compile with Hesiod support. */
+#undef ZEPHYR_USES_HESIOD
+
+/* Define to compile with Kerberos support. */
+#undef ZEPHYR_USES_KERBEROS
+
+/* Define to the directory containing Zephyr configuration files. */
+#define CONFDIR "/usr/local/etc/zephyr"
+
+/* Define to the directory containing Zephyr data files. */
+#define DATADIR "/usr/local/share/zephyr"
+
+/* Define to a signed 32-bit integral type. */
+#define INT_32_TYPE long
+
+/* Define if you have the System Resource Controller library. */
+#undef HAVE_SRC
+
+/* Define to "unsigned long" if your system headers don't. */
+#undef ulong
+
+/* Define to a temporary directory on your system. */
+#define FOUND_TMP "/var/tmp"
+
