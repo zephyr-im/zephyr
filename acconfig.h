86e9928ce70ed757c59605673a5a5699059d8d09
Greg Hudson <ghudson@mit.edu>
1995-07-07 17:53:19 +0000

    Use AC_CANONICAL_HOSTNAME for zhm. Define directories using Makefiles instead of header files, and use $(prefix) in the Makefiles.  Also, allow defaults.

--- acconfig.h
+++ acconfig.h
@@ -4,14 +4,8 @@
 /* Define to compile with Kerberos support. */
 #undef ZEPHYR_USES_KERBEROS
 
-/* Define to the directory containing Zephyr configuration files. */
-#define CONFDIR "/usr/local/etc/zephyr"
-
-/* Define to the directory containing Zephyr data files. */
-#define DATADIR "/usr/local/share/zephyr"
-
 /* Define to a signed 32-bit integral type. */
-#define INT_32_TYPE long
+#define ZEPHYR_INT32 long
 
 /* Define if you have the System Resource Controller library. */
 #undef HAVE_SRC
@@ -21,4 +15,7 @@
 
 /* Define to a temporary directory on your system. */
 #define FOUND_TMP "/var/tmp"
+
+/* Define to the type of the host system. */
+#define MACHINE_TYPE "unknown"
 
