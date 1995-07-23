f263a8527ed71e1743ff5e5f043ba89c3e553631
Greg Hudson <ghudson@mit.edu>
1995-07-23 02:29:13 +0000

    Use basic rather than extended regexps, for backwards- and cross-platform compatibility.

--- clients/zwgc/regexp.c
+++ clients/zwgc/regexp.c
@@ -35,7 +35,7 @@ int ed_regexp_match_p(test_string, pattern)
     int retval;
     char errbuf[512];
 
-    retval = regcomp(&RE, pattern, REG_EXTENDED|REG_NOSUB);
+    retval = regcomp(&RE, pattern, REG_NOSUB);
     if (retval != 0) {
 	regerror(retval, &RE, errbuf, sizeof(errbuf));
 	fprintf(stderr,"%s in regcomp %s\n",errbuf,pattern);
