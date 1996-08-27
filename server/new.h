0415965d8fd7420417895e189c9c79f67cfaf0a6
Greg Hudson <ghudson@mit.edu>
1996-08-27 12:28:13 +0000

    This file was deleted in the 8.0 source tree

--- server/new.h
+++
@@ -1,6 +0,0 @@
--*- text -*-
-
-This file is here just to confuse makedepend.  Normally, makedepend
-will not search the directories used for C++ header files; thus,
-anything specific to C++ won't be found unless we fake it out by
-putting a file in the working directory for it to find.
