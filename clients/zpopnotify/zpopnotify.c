f7cb597e3bbaebdbdf757e0cc10b247c378c615c
Marc Horowitz <marc@mit.edu>
1997-01-06 16:37:31 +0000

    don't specify a realm fro the sender; the library will fill one in.

--- clients/zpopnotify/zpopnotify.c
+++ clients/zpopnotify/zpopnotify.c
@@ -69,8 +69,7 @@ main(argc,argv)
 	lines[0] = myhost;
 	lines[1] = "You have new mail.";
 	
-	(void) strcpy(mysender,"pop@");
-	(void) strcat(mysender,ZGetRealm());
+	(void) strcpy(mysender,"pop");
 
 	for (i = 1; i < argc; i++) {
 	    (void) memset((char *)&notice, 0, sizeof(notice));
