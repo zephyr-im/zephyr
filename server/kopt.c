f595cb7747d8e49b91544ba895ef7c7faee8e4ae
Greg Hudson <ghudson@mit.edu>
1997-09-05 15:27:35 +0000

    decomp_ticket takes a des_key_sched, not a Sched.

--- server/kopt.c
+++ server/kopt.c
@@ -551,7 +551,7 @@ decomp_ticket(tkt, flags, pname, pinstance, prealm, paddress, session,
     char *sinstance;            /* Service instance */
     C_Block key;                /* Service's secret key
                                  * (to decrypt the ticket) */
-    Sched key_s;                /* The precomputed key schedule */
+    des_key_sched key_s;	/* The precomputed key schedule */
 {
     static int tkt_swap_bytes;
     unsigned char *uptr;
@@ -560,7 +560,7 @@ decomp_ticket(tkt, flags, pname, pinstance, prealm, paddress, session,
 #ifndef NOENCRYPTION
     /* Do the decryption */
     pcbc_encrypt((C_Block *)tkt->dat,(C_Block *)tkt->dat,
-                 (long) tkt->length,key_s.s,(C_Block *) key,0);
+                 (long) tkt->length,key_s,(C_Block *) key,0);
 #endif /* ! NOENCRYPTION */
 
     *flags = *ptr;              /* get flags byte */
