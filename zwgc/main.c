ff61301110d81f7d381cd43c351b1878f4b47444
Greg Hudson <ghudson@mit.edu>
1998-04-30 20:03:40 +0000

    Move files from datadir back to datadir/zephyr.

--- zwgc/main.c
+++ zwgc/main.c
@@ -120,7 +120,7 @@ static void read_in_description_file()
 
 /*    var_clear_all_variables(); <<<>>> */
 
-    sprintf(defdesc, "%s/%s", DATADIR, DEFDESC);
+    sprintf(defdesc, "%s/zephyr/%s", DATADIR, DEFDESC);
     input_file = locate_file(description_filename_override, USRDESC, defdesc);
     if (input_file)
       program = parse_file(input_file);
