7fb7d30f7b237605eee85ee8f426e920e3711ff8
Karl Ramm <kcr@mit.edu>
2001-04-01 20:09:11 +0000

    cleanup debhelper examples add debconf dependencies to appropriate packages don't install /etc/zephyr/acl/* and /etc/zephyr/zephyr.vars as executable

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the zephyr package
-#
-0 4	* * *	root	zephyr_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(zephyr):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="zephyr" command="/usr/bin/zephyr"
