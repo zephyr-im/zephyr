39885e5ba65feef6eb5ab5709b9d03b1bcaa7c5d
Greg Hudson <ghudson@mit.edu>
2001-04-10 19:28:19 +0000

    Reintroduce checksum enforcement changes.

--- server/dispatch.c
+++ server/dispatch.c
@@ -257,20 +257,7 @@ dispatch(notice, auth, who, from_server)
     char dbg_buf[BUFSIZ];
 #endif
 
-    /* Set "authflag" to 1 or 0 for handler functions.  Treat
-     * ZAUTH_CKSUM_FAILED as authentic except for sendit(), which is
-     * handled below. */
-    switch (auth) {
-      case ZAUTH_YES:
-      case ZAUTH_CKSUM_FAILED:
-	authflag = 1;
-	break;
-      case ZAUTH_FAILED:
-      case ZAUTH_NO:
-      default:
-	authflag = 0;
-	break;
-    }
+    authflag = (auth == ZAUTH_YES);
 
     if ((int) notice->z_kind < (int) UNSAFE ||
 	(int) notice->z_kind > (int) CLIENTACK) {
@@ -324,8 +311,6 @@ dispatch(notice, auth, who, from_server)
 	admin_notices.val++;
 	status = server_adispatch(notice, authflag, who, me_server);
     } else {
-	if (auth == ZAUTH_CKSUM_FAILED)
-	    authflag = 0;
 	if (!realm_bound_for_realm(ZGetRealm(), notice->z_recipient)) {
 	    cp = strchr(notice->z_recipient, '@');
 	    if (!cp ||
