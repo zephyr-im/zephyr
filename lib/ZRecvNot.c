0658dac39b4857fb02ff082577cb2e6942f98978
Derrick Brashear <shadow@dementix.org>
2004-08-26 21:34:27 -0400

    Avoid null pointer dereference in ZReceiveNotice
    Z_GetFirstComplete() can return NULL; in that case, we don't want to
    dereference the pointer it returns.
    
    Extracted from Andrew zephyr/064; authorship uncertain.

--- lib/ZRecvNot.c
+++ lib/ZRecvNot.c
@@ -28,6 +28,9 @@ ZReceiveNotice(ZNotice_t *notice,
     if ((retval = Z_WaitForComplete()) != ZERR_NONE)
 	return (retval);
 
+    if (!nextq)
+	return (ENOMEM);
+
     nextq = Z_GetFirstComplete();
 
     len = nextq->packet_len;
