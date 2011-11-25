54c7faba840799f5cbf3917c58063f439a7f224b
Karl Ramm <kcr@1ts.org>
2011-11-25 15:41:43 -0500

    fix pasteo, make git describe more verbose

--- get_vers.sh
+++ get_vers.sh
@@ -5,10 +5,11 @@ top_srcdir=${1:-`pwd`}
 if test -f ${top_srcdir}/VERSION; then
     VERSION=`cat ${top_srcdir}/VERSION`
 elif test -d ${top_srcdir}/.git; then
-    VERSION=`(cd $top_srcdir; git describe)`
+    VERSION=`(cd $top_srcdir; git describe --abbrev=6 --dirty)`
 fi
 
 if test -z "$VERSION"; then
     VERSION='FROM SPACE'
+fi
 
 echo $VERSION
