49152ad1189ce40c2629cab3236d968588b2fdb1
Karl Ramm <kcr@1ts.org>
2013-10-05 23:47:19 -0400

    try to expose the branch name

--- get_vers.sh
+++ get_vers.sh
@@ -6,6 +6,15 @@ if test -f ${top_srcdir}/VERSION; then
     VERSION=`cat ${top_srcdir}/VERSION`
 elif test -d ${top_srcdir}/.git; then
     VERSION=`(cd $top_srcdir; git describe --abbrev=6 --dirty | sed -e 's/%7E/~/')`
+
+    if test "$(git symbolic-ref HEAD)" != refs/heads/master; then
+        REF=$(git symbolic-ref --short HEAD)
+	if test "$REF" = "fatal: ref HEAD is not a symbolic ref"; then
+            VERSION="$VERSION: detached head"
+	else
+	    VERSION="$VERSION $(echo $REF | tr 'a-z-' 'A-Z ')"
+        fi
+    fi
 fi
 
 if test -z "$VERSION"; then
