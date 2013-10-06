279cbd919e3dd6dbfba025f833cef8f73afd01cf
Karl Ramm <kcr@1ts.org>
2013-10-05 21:42:50 -0400

    substitute ~ back out of the tag in get_vers

--- get_vers.sh
+++ get_vers.sh
@@ -5,7 +5,7 @@ top_srcdir=${1:-`pwd`}
 if test -f ${top_srcdir}/VERSION; then
     VERSION=`cat ${top_srcdir}/VERSION`
 elif test -d ${top_srcdir}/.git; then
-    VERSION=`(cd $top_srcdir; git describe --abbrev=6 --dirty)`
+    VERSION=`(cd $top_srcdir; git describe --abbrev=6 --dirty | sed -e 's/%7E/~/')`
 fi
 
 if test -z "$VERSION"; then
